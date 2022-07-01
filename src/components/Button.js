1231a4a1aaef4e9bfd67b4e694383ce650d63eed
lambaraa abdellah <abdellahlambaraa@gmail.com>
2022-07-01 15:18:09 +0100

    add

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,8 @@ import React from 'react'
 const button = (props) => {
   return (
     <div>
-        <input style={{backgroundColor:props.color}} type="submit" class="btn" value="add" />
+        <input style={{backgroundColor:props.color}} type="submit" class="btn" value="props.title" />
+        <button style={{backgroundColor:props.color}} class="btn">{props.title}</button>
     </div>
   )
 }
