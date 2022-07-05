897d28e50880891c9ddc2bcc8fe71f0ea95b2187
lambaraa abdellah <abdellahlambaraa@gmail.com>
2022-07-05 10:02:49 +0100

    add

--- src/App.js
+++ src/App.js
@@ -4,30 +4,35 @@ import Header from './components/Header';
 import './App.css';
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState } from "react"
 function App() {
-  const [showAddTask,setShowTask]=useState(false)
-  const [tasks,setTasks]=useState([]) 
-  useEffect(()=>{
-     
-  })
+  const [showAddTask, setShowTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+      console.log(data)
+    }
+    fetchTasks()
+
+  }, [])
   // const name='Abdellah'
   // const x=true*
   //delete task 
-  const deleteTask=(id)=>{
-    console.log('delete',id)
-    setTasks(tasks.filter((task)=>task.id !==id))
+  const deleteTask = (id) => {
+    console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
   }
   //toggle reminder
-  const toggleReminder=(id)=>{
+  const toggleReminder = (id) => {
     console.log(id)
-    setTasks(tasks.map((task)=>task.id===id?{...task,reminder:!task.reminder}:task))
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
   // add task
-  const addTask=(task)=>{
-    const id=Math.floor(Math.random()* 10000)+1
-    const newTask={id,...task}
-    setTasks([...tasks,newTask])
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
   }
   return (
     // <div className="App">
@@ -37,19 +42,19 @@ function App() {
     //     <h2>abdellah17lambaraa@gmail.com</h2>
     //     <Header title="full stack developper" />
     //   </header>
-      
-      
+
+
     // </div>
-  
+
     <div className='container'>
-      <Header title="Task tracker" onAdd={()=>setShowTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-     {tasks.length>0 ? <Tasks tasks={tasks} 
-     onDelete={deleteTask}
-     onToggle={toggleReminder}
-     />:'No Tasks to show'}
+      <Header title="Task tracker" onAdd={() => setShowTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks}
+        onDelete={deleteTask}
+        onToggle={toggleReminder}
+      /> : 'No Tasks to show'}
     </div>
-    
+
   );
 }
 
